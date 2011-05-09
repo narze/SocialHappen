0b2c00f6912648337eacd0815a6ebdfc1961554f
Noom <noom@figabyte.com>
2011-05-09 14:48:41 +0700

    Development server config

--- application/config/facebook.php
+++ application/config/facebook.php
@@ -1,7 +1,16 @@
 <?php
 
-	$config['facebook_app_id'] 			= '183244841718995';
-	$config['facebook_api_secret'] 		= '7cc75ac395a5953f7c37dd13b0a38f69';
+	$config['facebook_app_id'] 			= '108290455924296';
+	$config['facebook_api_secret'] 		= '868c038fcff0b5c10fd5c321ae6c23e2';
 	$config['facebook_default_scope']	= 'user_about_me,email,publish_stream'; // E.G 'read_stream,birthday,users_events,rsvp_event'
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
 	$config['facebook_callback_url'] 	= 'socialhappen'; // ralate with site_url(); 
+	
+	
+/* local
+	$config['facebook_app_id'] 			= '118450098237355';
+	$config['facebook_api_secret'] 		= '868c038fcff0b5c10fd5c321ae6c23e2';
+	$config['facebook_default_scope']	= 'user_about_me,email,publish_stream'; // E.G 'read_stream,birthday,users_events,rsvp_event'
+	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
+	$config['facebook_callback_url'] 	= 'socialhappen'; // ralate with site_url(); 
+ */	
