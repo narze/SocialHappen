a231223cadd90e6fc8b06eee2ee1d279e5d879e5
Noom <noom@figabyte.com>
2011-05-18 19:54:26 +0700

    Rename

--- application/controllers/test/company_test.php
+++ application/controllers/test/company_test.php
@@ -1,6 +1,6 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-class Api extends CI_Controller {
+class Company_test extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->library('unit_test');
