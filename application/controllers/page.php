07320e0d103fcdf946624c0ae66b2563eeb97bcf
Noom <noom@figabyte.com>
2011-05-19 18:43:32 +0700

    Page : get_campaign_list

--- application/controllers/page.php
+++ application/controllers/page.php
@@ -14,12 +14,22 @@ class Page extends CI_Controller {
 	 * JSON : get installed app list
 	 * @param $page_id
 	 * @author Manassarn M.
-	 * 
 	 */
 	function json_get_installed_app_list($page_id = NULL){
 		$this->load->model('installed_apps_model','installed_apps');
 		$apps = $this->installed_apps->get_installed_apps($page_id);
 		echo json_encode($apps);
+	}
+	
+	/**
+	 * JSON : get campaign list
+	 * @param $page_id
+	 * @author Manassarn M.
+	 */
+	function json_get_campaign_list($page_id = NULL){
+		$this->load->model('campaign_model','campaigns');
+		$campaigns = $this->campaigns->get_campaigns($page_id);
+		echo json_encode($campaigns);
 	}
 	
 }
--- application/controllers/test/page_test.php
+++ application/controllers/test/page_test.php
@@ -11,7 +11,12 @@ class Page_test extends CI_Controller {
 	}
 
 	function index(){
-		$this->json_get_installed_app_list_test();
+		$class_methods = get_class_methods($this);
+		foreach ($class_methods as $method) {
+    		if(preg_match("/(_test)$/",$method)){
+    			$this->$method();
+    		}
+		}
 	}
 	
 	/**
@@ -21,7 +26,7 @@ class Page_test extends CI_Controller {
 	function json_get_installed_app_list_test(){
 		$content = file_get_contents(base_url().'page/json_get_installed_app_list/1');
 		$array = json_decode($content);
-		$this->unit->run($array, 'is_array', 'Installed app page list returns json correctly');
+		$this->unit->run($array, 'is_array', 'Installed app list returns json correctly');
 		$this->unit->run($array[0], 'is_object', 'First row');
 		$this->unit->run($array[0]->app_install_id,'is_string','app_install_id');
 		$this->unit->run($array[0]->company_id,'is_string','company_id');
@@ -31,6 +36,35 @@ class Page_test extends CI_Controller {
 		$this->unit->run($array[0]->page_id,'is_string','page_id');
 		$this->unit->run($array[0]->app_install_secret_key,'is_string','app_install_secret_key');
 		$this->unit->run(count((array)$array[0]) == 7, 'is_true', 'number of column');
+	}
+	
+	/**
+	 * Tests json_get_campaign_list()
+	 * @author Manassarn M.
+	 */
+	function json_get_campaign_list_test(){
+		$content = file_get_contents(base_url().'page/json_get_campaign_list/1');
+		$array = json_decode($content);
+		$this->unit->run($array, 'is_array', 'Campaign list returns json correctly');
+		$this->unit->run($array[0], 'is_object', 'First row');
+		$this->unit->run($array[0]->campaign_id,'is_string','campaign_id');
+		$this->unit->run($array[0]->app_install_id,'is_string','app_install_id');
+		$this->unit->run($array[0]->campaign_name,'is_string','campaign_name');
+		$this->unit->run($array[0]->campaign_detail,'is_string','campaign_detail');
+		$this->unit->run($array[0]->campaign_status_id,'is_string','campaign_status_id');
+		$this->unit->run($array[0]->campaign_status_name,'is_string','campaign_status_name');
+		$this->unit->run($array[0]->campaign_active_member,'is_string','campaign_active_member');
+		$this->unit->run($array[0]->campaign_all_member,'is_string','campaign_all_member');
+		$this->unit->run($array[0]->campaign_start_timestamp,'is_string','campaign_start_timestamp');
+		$this->unit->run($array[0]->campaign_end_timestamp,'is_string','campaign_end_timestamp');
+		
+		$this->unit->run($array[0]->company_id,'is_string','company_id');
+		$this->unit->run($array[0]->app_id,'is_string','app_id');
+		$this->unit->run($array[0]->app_install_status,'is_string','app_install_status');
+		$this->unit->run($array[0]->app_install_date,'is_string','app_install_date');
+		$this->unit->run($array[0]->page_id,'is_string','page_id');
+		$this->unit->run($array[0]->app_install_secret_key,'is_string','app_install_secret_key');
+		$this->unit->run(count((array)$array[0]) == 16, 'is_true', 'number of column');
 	}
 	
 }
