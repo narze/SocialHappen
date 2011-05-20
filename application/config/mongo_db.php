9df3b625ef7114bbef4f641821f14aae841b760f
Noom <noom@figabyte.com>
2011-05-20 17:04:04 +0700

    Added Mongo DB

---
+++ application/config/mongo_db.php
@@ -0,0 +1,9 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['mongo_host'] = 'localhost';
+$config['mongo_port'] = 27017;
+$config['mongo_db'] = 'socialhappen';
+$config['mongo_user'] = 'socialhappen';
+$config['mongo_pass'] = 'figyfigy';
+$config['mongo_persist'] = TRUE;
+$config['mongo_persist_key'] = 'ci_mongo_persist';
