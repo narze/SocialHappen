726dbe456c6c97dd223efbc0248239036ef9e3c0
Noom <noom@figabyte.com>
2011-05-30 20:49:36 +0700

    Added encryption key for sessions

--- application/config/config.php
+++ application/config/config.php
@@ -224,7 +224,7 @@ $config['cache_path'] = '';
 | MUST set an encryption key.  See the user guide for info.
 |
 */
-$config['encryption_key'] = '';
+$config['encryption_key'] = 'a02k#DL!4sgt0#$0;1z-d$dfG+_)!(A!';
 
 /*
 |--------------------------------------------------------------------------
