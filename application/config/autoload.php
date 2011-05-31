923e87aa86f6eeedc46b8fe0ed17eb0eb09c07f5
Noom <noom@figabyte.com>
2011-05-31 16:14:52 +0700

    autoload facebook library

--- application/config/autoload.php
+++ application/config/autoload.php
@@ -52,7 +52,7 @@ $autoload['packages'] = array(APPPATH.'third_party');
 |	$autoload['libraries'] = array('database', 'session', 'xmlrpc');
 */
 
-$autoload['libraries'] = array('database','app_url','curl');
+$autoload['libraries'] = array('database','app_url','curl','facebook');
 
 
 /*
