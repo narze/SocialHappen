075407c6f7d08e72483a252affd0d13fb0a3839e
Noom <noom@figabyte.com>
2011-06-06 18:30:43 +0700

    Company image and name view

---
+++ application/views/company/company_image_and_name.php
@@ -0,0 +1,4 @@
+<div id="company-name-and-image">
+	<div class="company-image"><img src="<?php echo issetor($company['company_image'],'No company image specified'); ?>" /></div>
+	<div class="company-name"><?php echo issetor($company['company_name'],'No company name specified'); ?></div>
+</div>
