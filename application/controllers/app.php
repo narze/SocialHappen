2c3505d4c24b2199f4f7655debe4976a2ca5d8f7
POP <POP@.(none)>
2011-06-10 22:14:46 +0700

    fix bug in get_app_by_api_key

--- application/controllers/app.php
+++ application/controllers/app.php
@@ -113,7 +113,7 @@ class App extends CI_Controller {
 	 */
 	function json_get_app_by_api_key($fb_app_api_key){
 		$this->load->model('app_model','app');
-		$app = $this->app->get_app_by_fb_app_api_key($fb_app_api_key);
+		$app = $this->app->get_app_by_api_key($fb_app_api_key);
 		echo json_encode($app);
 	}
 }
