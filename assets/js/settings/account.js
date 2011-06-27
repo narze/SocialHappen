3631b7e2b658bb5ef93d01d5a99f022a1d885a75
Noom <noom@figabyte.com>
2011-06-27 18:55:33 +0700

    ajaxForm jquery plugin in account settings page Fixes footer

--- assets/js/settings/account.js
+++
@@ -1,9 +0,0 @@
-$(function(){
-	$('form.account').live('submit',function(){
-		formData = $(this).serializeArray();					
-		$.post(base_url+"settings/account/"+param_id,formData,function(returnData){
-			$('.form').replaceWith(returnData);
-		});
-		return false;
-	});
-});
