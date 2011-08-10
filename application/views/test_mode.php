bfded60513f51aca22372f5d27a6a539ba1eacf3
Noom <noom@figabyte.com>
2011-08-10 22:05:40 +0700

    Added test mode for developers & testers

---
+++ application/views/test_mode.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>SocialHappen Test mode</title>
+	<?php echo link_tag('css/style.css'); ?>
+</head>
+<body>
+<h1>You've reached SH beta page!</h1>
+<?php
+echo form_open('');
+?>
+		<ul>
+			<li>
+				<?=form_label('Y U NO PASS', 'test_mode_password', array(
+				'style' => 'color: #000;',
+				));?>
+				<?=form_password(array(
+						  'name'        => 'test_mode_password',
+						  'id'          => 'test_mode_password',
+						  'value'       => set_value('test_mode_password'),
+						  'maxlength'   => '30',
+						  'size'        => '50',
+						  'class'		=> (form_error('test_mode_password')) ? 'error' : '',
+				))?>
+				<?php echo form_error('test_mode_password'); ?>
+			</li>
+		</ul>
+		
+		<div class="button-style">
+		<p>
+		<?php echo form_submit(array(
+							'name' => 'submit', 
+							'value' => 'GO!'
+							)); ?>
+		</p>
+		</div>
+		<?=form_close()?>
+		<?php die(); ?>
+</body>
+</html>
