b159f8a2433fc8b40ff164837c516acd009a52f6
Weerapat P <vinboxx@gmail.com>
2011-08-16 17:13:14 +0700

    Tab footer

--- application/views/tab/footer.php
+++ application/views/tab/footer.php
@@ -1,5 +1,5 @@
 		<div class="footer">
-			<p>Copyright ? 2011 SocialHappen, All Rights Reserved</p>
+			<p>Copyright &copy; 2011 SocialHappen, All Rights Reserved</p>
 			<p>Powered by <a href="<?php echo base_url();?>">SocialHappen</a></p>
 			<p class="logo">logo</p>
 	    </div>
