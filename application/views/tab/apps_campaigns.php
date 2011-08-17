b1aaebe25a6c24491c6dd54e080cde046983ea40
Weerapat P <tong@figabyte.com>
2011-08-17 20:32:49 +0700

    Fix apps campaign view

--- application/views/tab/apps_campaigns.php
+++ application/views/tab/apps_campaigns.php
@@ -1,27 +1,34 @@
- <ul>
-	<?php if(isset($campaigns)) :
+ <ul><?php 
+	if(isset($campaigns)) :
 		foreach($campaigns as $campaign): ?>
-		<li>
+		  <li>
             <div>
               <p class="pic"><img src="<?php echo imgsize($campaign['campaign_image'],'normal');?>" alt="" /></p>
               <h2><?php echo $campaign['campaign_name'];?></h2>
               <p><?php echo $campaign['campaign_detail'];?></p>
               <p class="link"><a href="#">read more</a></p>
             </div>
-            <div>
-				<h2>Remaining Time</h2>
-					<?php if(date('Y-m-d H:i:s') < $campaign['campaign_end_timestamp'] ) { ?>
-						<div style="display: none;" class="campaign-end-time"><?php echo $campaign['campaign_end_timestamp'];?></div>
-						<?php if(!$is_admin) { ?>
-							<p><a class="bt-join" href="#"><span>Join</span></a></p>
-						<?php } ?> 
-					<?php } else { ?>
-					<p><a class="bt-time_up"><span>Time's up</span></a></p>
-					<?php } ?> 
-            </div>
-          </li>
-		<?php endforeach; 
-		endif;?>
+            <div><?php 
+					if (date('Y-m-d H:i:s') < $campaign['campaign_end_timestamp'] ) : ?>
+						<h2>Remaining Time</h2>
+						<div style="display: none;" class="campaign-end-time"><?php echo $campaign['campaign_end_timestamp'];?></div><?php 
+						if($is_user) : ?>
+							<p><a class="bt-join" href="#"><span>Join</span></a></p><?php 
+						endif;
+					else : ?>
+						<p><a class="bt-time_up"><span>Time's up</span></a></p><?php 
+					endif;
+					
+					if ($is_admin) :?>			
+					  <ul>
+						<li><?php echo $campaign['campaign_active_member'];?><span>Today</span></li>
+						<li><?php echo $campaign['campaign_all_member']; ?><span>All</span></li>
+					  </ul><?php 
+					endif; ?>  
+            </div>			
+          </li><?php 
+		endforeach; 
+	endif;?>
 	  
 		<?php if(isset($apps)) :
 		foreach($apps as $app): ?>
