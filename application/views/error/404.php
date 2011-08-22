30f3d19d81a0ef0900dfb30f3cebf5ceaba55d69
Weerapat P <tong@figabyte.com>
2011-08-22 12:56:27 +0700

    Add 'Error' controller and views

---
+++ application/views/error/404.php
@@ -0,0 +1,5 @@
+<div class="wrapper-details">
+	<h1>That page doesn't exist!</h1>
+		<div class="error-detail">You may have mis-typed the URL, Please check your spelling.</div>
+	</p>
+</div>
---
+++ application/views/error/error_view.php
@@ -0,0 +1,9 @@
+{header}
+{breadcrumb}
+<div class="wrapper-content">
+	<div>
+		{error}
+	</div>
+	<div class="bottom"><!--bottom--></div>
+</div>
+{footer}
