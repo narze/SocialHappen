4be9615f9d46efdf5e8e120086e5e278869e6f84
Weerapat P <tong@figabyte.com>
2011-08-29 15:22:53 +0700

    Add "Setting>package" page

--- application/controllers/settings.php
+++ application/controllers/settings.php
@@ -347,8 +347,48 @@ class Settings extends CI_Controller {
 	
 	function package($user_id = NULL){
 		$this->socialhappen->ajax_check();
-		if($user_id && $user_id == $this->socialhappen->get_user_id()){	
-			$this->load->view('settings/package',array());
+		if($user_id && $user_id == $this->socialhappen->get_user_id())
+		{
+			//Get all orders
+			$this->load->model('order_model','orders');
+			$orders = $this->orders->get_orders_by_user_id($user_id, $limit = NULL, $offset = NULL);
+			$this->load->model('order_items_model','order_items');
+			foreach($orders as &$order)
+			{
+				$items = $this->order_items->get_order_items_by_order_id($order['order_id']);
+				$order['package_name'] = $items[0]['item_name'];
+			}
+			arsort($orders); //reverse order
+			
+			//Get current package
+			$this->load->model('package_users_model','package_users');
+			$current_package = $this->package_users->get_package_by_user_id($user_id);
+			
+			//Get package apps
+			$this->load->model('package_apps_model','package_apps');
+			$apps = $this->package_apps->get_apps_by_package_id($current_package['package_id']);
+			
+			//Count user companies
+			$this->load->model('company_model','companies');
+			$user_companies = $this->companies->get_companies_by_user_id($user_id);
+			
+			//Count user pages
+			$this->load->model('user_pages_model','user_pages');
+			$user_pages = $this->user_pages->get_user_pages_by_user_id($user_id);
+			
+			//Count members
+			
+			$members = $this->package_users->count_user_members_by_user_id($user_id);
+			
+			$data = array(
+				'orders' => $orders,
+				'current_package' => $current_package,
+				'user_companies' => count($user_companies),
+				'user_pages' => count($user_pages),
+				'members' => $members,
+				'apps' => $apps
+			);
+			$this->load->view('settings/package',$data);
 		}
 	}
 	
