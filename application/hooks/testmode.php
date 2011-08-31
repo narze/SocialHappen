c4534dfc381b346bfe46a589e4e327d4822abd7f
Noom <noom@figabyte.com>
2011-08-31 11:54:08 +0700

    Skip test mode if request from paypal

--- application/hooks/testmode.php
+++ application/hooks/testmode.php
@@ -5,22 +5,24 @@ class TestMode {
 		$ci =& get_instance();
 		if($ci->config->item('sh_test_mode')){
 		  $ci->load->helper('form');
-			if(isset($_SERVER['HTTP_USER_AGENT']) //letting calls from file_get_contents() bypass
-			&& (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')){ //letting calls from ajax bypass
-				
-				$ci->load->library('session');
-				if(!$ci->session->userdata('test_mode')){
-					$password = $ci->input->post('test_mode_password');
-					if($password != $ci->config->item('sh_test_mode_password')){
-						$ci->load->view('test_mode');
-					} else {
-						$userdata = array(
-							'test_mode' => TRUE
-						);
-						$ci->session->set_userdata($userdata);
+			if(!in_array($_SERVER['REMOTE_ADDR'], array('216.113.188.202','216.113.188.203','216.113.188.204','66.211.170.66')){ //Skip if from paypal
+				if(isset($_SERVER['HTTP_USER_AGENT']) //letting calls from file_get_contents() bypass
+				&& (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')){ //letting calls from ajax bypass
+					
+					$ci->load->library('session');
+					if(!$ci->session->userdata('test_mode')){
+						$password = $ci->input->post('test_mode_password');
+						if($password != $ci->config->item('sh_test_mode_password')){
+							$ci->load->view('test_mode');
+						} else {
+							$userdata = array(
+								'test_mode' => TRUE
+							);
+							$ci->session->set_userdata($userdata);
+						}
+					} else if($ci->input->get('test_mode_logout')){
+						$this->logout();
 					}
-				} else if($ci->input->get('test_mode_logout')){
-					$this->logout();
 				}
 			}
 		}
