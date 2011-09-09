f0e17be0d189b98fbf1666a850982023b0b0ddc8
Weerapat P <tong@figabyte.com>
2011-09-09 16:45:24 +0700

    Change Paypal API key

--- application/config/paypal_pro.php
+++ application/config/paypal_pro.php
@@ -3,9 +3,9 @@ if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['Sandbox'] = TRUE;
 $config['APIVersion'] = '66.0';
-$config['APIUsername'] = $config['Sandbox'] ? 'figy_1314089794_biz_api1.gmail.com' : 'PRODUCTION_USERNAME_GOES_HERE';
-$config['APIPassword'] = $config['Sandbox'] ? '1314089846' : 'PRODUCTION_PASSWORD_GOES_HERE';
-$config['APISignature'] = $config['Sandbox'] ? 'A3WhQeCZ0By9hfbGJJPDneGDP2lBA66R9MhbzxlTps.U-Qjoh4ECl0gK' : 'PRODUCTION_SIGNATURE_GOES_HERE';
+$config['APIUsername'] = $config['Sandbox'] ? 'figy_1315552003_biz_api1.figabyte.com' : 'PRODUCTION_USERNAME_GOES_HERE';
+$config['APIPassword'] = $config['Sandbox'] ? '1315552082' : 'PRODUCTION_PASSWORD_GOES_HERE';
+$config['APISignature'] = $config['Sandbox'] ? 'A7F5dPvwf.EFCLaVItlooCYR6N0GAODd2mXV7lY5PfQrUE4.iwKZUOZc' : 'PRODUCTION_SIGNATURE_GOES_HERE';
 
 /* End of file paypal_pro.php */
 /* Location: ./system/application/config/paypal_pro.php */
