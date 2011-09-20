d56237c425034852b420f6a5f73f0159a40dbd56
Weerapat P <tong@figabyte.com>
2011-09-20 17:47:53 +0700

    Fixed page installation

--- application/views/page/page_view.php
+++ application/views/page/page_view.php
@@ -4,10 +4,20 @@
 <div class="wrapper-content">
 	<div>
 		{page_profile}
-		{page_tabs}<?php 
-		if($is_package_over_the_limit) { ?>
-			{package_limited}<?php 
-		} else { ?>
+		<?php 
+		if($page_installed == 0) 
+		{ ?>
+			<div class="wrapper-details">
+				<div class="notice warning">
+					<h2>Page installation is not complete</h2>
+					Please, go to facebook to complete the action. <a class="gotofacebook-link" href="http://www.facebook.com/add.php?api_key=<?php echo $app_facebook_api_key; ?>&pages=1&page=<?php echo $facebook_page_id; ?>" >Go to facebook</a>
+				</div>
+				<br />
+			</div><?php 
+		} 
+		else 
+		{ ?>
+			{page_tabs}
 			{page_apps}
 			{page_campaigns}
 			{page_users}
