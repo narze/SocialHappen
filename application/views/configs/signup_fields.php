627815935798077181679a4bab0907bc447e0658
Noom <noom@figabyte.com>
2011-09-23 16:12:18 +0700

    Prepare merge

--- application/views/configs/signup_fields.php
+++ application/views/configs/signup_fields.php
@@ -7,7 +7,7 @@
 	{
 		foreach($fields as $key => $field)
 		{ ?>
-			<li id="field-<?php echo $key?>">
+			<li class="<?php echo $key?>">
 				<label><?php echo $field['label']; ?>: </label><?php 
 				if(isset($field['items'])) {
 					foreach ($field['items'] as $item)
@@ -31,55 +31,58 @@
 	}?>
 	</ul>
 	
-	<a class="bt-add-field-from-list">Add field by choose from the list</a>
+	<a href="#default-fields" id="bt-add-field-from-list" class="bt-add-field-from-list">Add field by choose from the list</a>
 	<a class="bt-create-own-field">Create your own field</a>
 	<?php 
 	echo form_submit('submitForm', 'Submit', 'class="bt-update"');
 	echo form_close(); ?>
+	<div id="no-submit" style="display:none"><ul></ul></div>
 	
 </div>
 
 <!-- Pop up default field-->
-<div id="default-fields" style="display:none">
-	<h2>Add more field</h2>
-	<p>Choose from the list below</p>
-	<ul><?php
-	foreach($fields as $field)
-	{ ?>
-		<li>
-			<input type="checkbox" name="<?php echo $field['name']?>" id="<?php echo $field['name']?>" />
-			<label for="<?php echo $field['name']?>"><?php echo $field['label']; ?></label>
-		</li><?php
-	} ?>
-	</ul>
-	<a class="bt-add-these-field">Add these fields to the sign up form</a>
-</div>
+<div id="fancy" style="display:none">
+	<div id="default-fields">
+		<h2>Add more field</h2>
+		<p>Choose from the list below</p>
+		<ul><?php
+		foreach($fields as $field)
+		{ ?>
+			<li>
+				<input type="checkbox" name="<?php echo $field['name']?>" id="<?php echo $field['name']?>" />
+				<label for="<?php echo $field['name']?>"><?php echo $field['label']; ?></label>
+			</li><?php
+		} ?>
+		</ul>
+		<a class="bt-add-these-field">Add these fields to the sign up form</a>
+	</div>
 
-<!-- Pop up custom field-->
-<div id="custom-fields" style="display:none">
-	<h2>Create your own field</h2>
-	<ul>
-		<li>
-			<label>Field title: </label><input type="text" name="new-field" />
-			<label>Field type</label> 
-				<select name="field-type">
-					<option selected="selected">Text</option>
-					<option>Paragraph Text</option>
-					<option>Multiple choices</option>
-					<option>Check box</option>
-					<option>Dropdown</option>
-				</select>
-			<input type="checkbox" name="new-field-required" /> <label>Required</label> | 
-			<a class="bt-remove-field">Remove</a>
-			<div class="options">
-				<input type="text" value="Option 1" /><br />
-				<input type="text" value="Click to add option" disabled="disabled" /> or add <a class="add-other">"Other"</a>
-			</div>
-		</li>
-	</ul>
-	<a class="add-more-field">Add more field</a>
-	
-	<p style="text-align:center">
-		<a class="bt-add-these-custom-field">Add this fields the sign up form</a>
-	</p>
+	<!-- Pop up custom field-->
+	<div id="custom-fields">
+		<h2>Create your own field</h2>
+		<ul>
+			<li>
+				<label>Field title: </label><input type="text" name="new-field" />
+				<label>Field type</label> 
+					<select name="field-type">
+						<option selected="selected">Text</option>
+						<option>Paragraph Text</option>
+						<option>Multiple choices</option>
+						<option>Check box</option>
+						<option>Dropdown</option>
+					</select>
+				<input type="checkbox" name="new-field-required" /> <label>Required</label> | 
+				<a class="bt-remove-field">Remove</a>
+				<div class="options">
+					<input type="text" value="Option 1" /><br />
+					<input type="text" value="Click to add option" disabled="disabled" /> or add <a class="add-other">"Other"</a>
+				</div>
+			</li>
+		</ul>
+		<a class="add-more-field">Add more field</a>
+		
+		<p style="text-align:center">
+			<a class="bt-add-these-custom-field">Add this fields the sign up form</a>
+		</p>
+	</div>
 </div>
