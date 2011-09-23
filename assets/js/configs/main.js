41bbcfd1e9caec325c4f4262d9749b4fb7b194de
Noom <noom@figabyte.com>
2011-09-23 20:46:16 +0700

    Update signup fields config

--- assets/js/configs/main.js
+++ assets/js/configs/main.js
@@ -19,12 +19,13 @@ $(function(){
 			$.fancybox({
 				content: $('#default-fields').appendTo(this),
 				onCleanup: function(){ 
-					$('#default-fields').clone().appendTo($('#fancy'));
+					$('#default-fields').clone().appendTo($('#fancy')); //Clone to make ux better, it'll be remove by fancybox
 				}
 			});
 			
 			//Add/remove select/deselect fields
-			$('a.bt-add-these-field').click(function(){
+			$('#default-fields a.bt-apply-to-signup-form').click(function(){
+				$.fancybox.close();
 				var selected = $('#default-fields li input:checkbox:checked').map(function(){
 					return this.name;
 				}).get();
@@ -43,23 +44,78 @@ $(function(){
 	
 		$('a.bt-create-own-field').live('click', function(){
 			$.fancybox({
-				content: $('#custom-fields').clone().appendTo(this)
+				content: $('#custom-fields').appendTo(this),
+				onCleanup: function(){ 
+					$('#custom-fields').clone().appendTo($('#fancy'));
+				}
 			});
 			
-			$('a.bt-add-these-custom-field').live('click', add_new_fields);
+			$('#custom-fields ul li div.options ul li.add-option input').click(function(){
+				add_more_option(this).find('input[type="text"]').val('').focus();
+			});
+			$('#custom-fields ul li div.options ul li.add-option a.add-other').click(function(){
+				add_more_option(this).find('input[type="text"]').val('Other');
+			});
+			$('#custom-fields ul li div.options ul li a.bt-remove-option').live('click',function(){
+				if($(this).parent().siblings('li.option').length > 0){ //if at least one option left
+					$(this).parent().remove(); //remove option
+				}
+			});
+			
+			$('#custom-fields ul li select[name="field-type"]').live('change', function(){
+				var selected_type = $(this).find('option:selected').val();
+				var old_value = $(this)
+				switch (selected_type){
+					case 'radio':
+						$(this).parents('#custom-fields ul li').find('div.options').show().find('input.option-type').replaceWith('<input type="radio" name="option-type-example" class="option-type" />');
+					break;
+					case 'checkbox':
+						$(this).parents('#custom-fields ul li').find('div.options').show().find('input.option-type').replaceWith('<input type="checkbox" name="option-type-example" class="option-type" />');	
+					break;
+					case 'textarea':
+						$(this).parents('#custom-fields ul li').find('div.options').hide();
+					break;
+					case 'text':
+					default:
+						$(this).parents('#custom-fields ul li').find('div.options').hide();
+					break;
+				}
+			});
+			
+			$('#custom-fields ul li a.bt-remove-field').live('click',function(){
+				if($(this).parents('li.field').siblings('li.field').length == 0){ //if no option left
+					add_more_field();
+				}$(this).parents('li.field').remove();
+			});
+			$('#custom-fields a.add-more-field').click(add_more_field);
+			$('#custom-fields p.apply a.bt-apply-to-signup-form').click(function (){ alert('aa');
+				add_new_fields();
+				$.fancybox.close();
+			});
 		});
 		
 		function add_new_fields(){
-			var new_fields = $('#custom-fields ul li');
-			for(i in new_fields){
+			var new_fields = $('#custom-fields ul.fields li.field').get();
+			for(i in new_fields){console.log(new_fields[i]);
 				var this_field = $(new_fields[i]);
-				var title = this_field.find('input[name="new-field"]').val();
-				var type = this_field.find('input').attr('type');
-				var required = this_field.find('input[name="new-field-required"]').val();
-				var options = this_field.find('div.options').map(function(){
-					
+				var data = new Object();
+				data.title = this_field.find('input[name="new-field"]').val();
+				data.name = data.title; //TODO : underscore & lowercase
+				data.type = this_field.find('input select[name="field-type"] option:selected').val();
+				data.required = this_field.find('input[name="new-field-required"]').val();
+				data.options = this_field.find('div.options ul li.option').map(function(){
+					//TODO : get options
 				});
+				console.log(data);
 			}
+		}
+		
+		function add_more_option(obj){
+			return $(obj).parent().prev().clone(true).insertBefore($(obj).parent()); //Clone to make ux better, it'll be remove by fancybox
+		}
+		
+		function add_more_field(){
+			$('#custom-fields ul.fields-template').clone().children('li').appendTo('#custom-fields ul.fields').find('input[type="text"][name="new-field"]').focus();
 		}
 	}
 	
