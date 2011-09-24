47b99bae9f373d255488d806183fb1aeea70f138
Metwara Narksook <hybridknight@gmail.com>
2011-09-24 13:20:38 +0700

    add custom field config app to page

--- assets/js/company/company_dashboard.js
+++ assets/js/company/company_dashboard.js
@@ -430,6 +430,12 @@ function show_installed_app_in_page(page_id,facebook_page_id) {
 			$(".notice").hide();
 			var ul_element=$(".left-panel").find('.dragging-app div').find('ul');
 			ul_element.append('<li class="add-app"></li>');
+			ul_element.append('<li><p><img class="app-image" src="'+imgsize('','normal')+'" alt="" width="64" height="64" />'
+				+'<span class="button">'
+				+'<a class="bt-update_app" href="'+base_url+'configs?p='+page_id+'&c=signup_fields"><span>Update</span></a>'
+				+'<a class="bt-setting_app" href="'+base_url+'configs?p='+page_id+'&c=signup_fields"><span>Setting</span></a>'
+				+'</span>'
+				+'</p><p class="appname">'+ 'NAME' +'</p><input type="hidden" class="app_install_id" value="'+0+'" /></li>');
 			for(i in json) {
 				ul_element.append('<li><p><img class="app-image" src="'+imgsize(json[i].app_image,'normal')+'" alt="" width="64" height="64" />'
 				+'<span class="button">'
