6963b6a65889fbbada0b35cdb94aeea693e062f8
Metwara Narksook <hybridknight@gmail.com>
2011-09-25 10:30:52 -0700

    add audit_stat_limit_lib and test cases

---
+++ application/controllers/test/audit_stat_limit_lib_test.php
@@ -0,0 +1,253 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Audit_stat_limit_lib_test extends CI_Controller {
+	var $audit;
+	var $audit_stat_limit_lib;
+	
+	function __construct(){
+		parent::__construct();
+		$this->load->library('unit_test');
+		$this->load->model('audit_stats_model','stats');
+		$this->load->library('audit_stat_limit_lib');
+	}
+	
+	function index(){
+		$class_methods = get_class_methods($this);
+		echo 'Functions : '.(count(get_class_methods($this->stats))-3).' Tests :'.count($class_methods);
+		foreach ($class_methods as $method) {
+    		if(preg_match("/(_test)$/",$method)){
+    			$this->$method();
+    		}
+		}
+	}
+	
+	function __destruct(){
+		$this->unit->report_with_counter();
+	}
+	
+	function start_test(){
+		$this->stats->drop_collection();
+	}
+	
+	function create_index_test(){
+		$this->audit_stat_limit_lib->create_index();
+	}
+	
+	function add_invalid_test(){
+		$user_id = 1;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no);
+		$result = $this->audit_stat_limit_lib->add($user_id, NULL, $app_install_id, $campaign_id);
+		$this->unit->run($result, 'is_false', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 0, 'add', print_r($result, TRUE));
+		
+		$data = array('user_id' => $user_id);
+		$result = $this->audit_stat_limit_lib->add($user_id, $action_no, NULL, $campaign_id);
+		$this->unit->run($result, 'is_false', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 0, 'add', print_r($result, TRUE));
+		
+		$data = array();
+		$result = $this->audit_stat_limit_lib->add(NULL, $action_no, $app_install_id, $campaign_id);
+		$this->unit->run($result, 'is_false', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 0, 'add', print_r($result, TRUE));
+	}
+	
+	function add_test(){
+		$user_id = 1;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id);
+		$result = $this->audit_stat_limit_lib->add($user_id, $action_no, $app_install_id, $campaign_id);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 1, 'add', print_r($result, TRUE));
+		
+		$user_id = 2;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'campaign_id' => $campaign_id);
+		$result = $this->audit_stat_limit_lib->add($user_id, $action_no, $app_install_id, $campaign_id);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 2, 'add', print_r($result, TRUE));
+		
+		$user_id = 3;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'campaign_id' => $campaign_id);
+		$result = $this->audit_stat_limit_lib->add($user_id, $action_no, $app_install_id, $campaign_id);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$result = $this->stats->list_stat();
+		$this->unit->run(count($result), 3, 'add', print_r($result, TRUE));
+	}
+
+	function count_test(){
+		$user_id = 1;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 3600);
+		$this->unit->run($result, 1, 'count', print_r($result, TRUE));
+		
+		$user_id = 2;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 3600);
+		$this->unit->run($result, 1, 'count', print_r($result, TRUE));
+		
+		$user_id = 3;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 3600);
+		$this->unit->run($result, 1, 'count', print_r($result, TRUE));
+		
+		$user_id = 99;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 3600);
+		$this->unit->run($result, 0, 'count', print_r($result, TRUE));
+	}
+
+	function count2_test(){
+		$this->stats->drop_collection();
+		$this->stats->create_index();
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		date_default_timezone_set('Asia/Bangkok');
+		$timestamp = time() - 1000;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'campaign_id' => $campaign_id,
+									'timestamp' => $timestamp);
+		$result = $this->stats->add_stat($data);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		date_default_timezone_set('Asia/Bangkok');
+		$timestamp = time() - 10000;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'campaign_id' => $campaign_id,
+									'timestamp' => $timestamp);
+		$result = $this->stats->add_stat($data);
+		$result = $this->stats->add_stat($data);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		date_default_timezone_set('Asia/Bangkok');
+		$timestamp = time() - 100000;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'campaign_id' => $campaign_id,
+									'timestamp' => $timestamp);
+		$result = $this->stats->add_stat($data);
+		$result = $this->stats->add_stat($data);
+		$result = $this->stats->add_stat($data);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = NULL;
+		date_default_timezone_set('Asia/Bangkok');
+		$timestamp = time() - 1000;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'timestamp' => $timestamp);
+		$result = $this->stats->add_stat($data);
+		$result = $this->stats->add_stat($data);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 70;
+		$campaign_id = NULL;
+		date_default_timezone_set('Asia/Bangkok');
+		$timestamp = time() - 1000;
+		$data = array('user_id' => $user_id,
+									'action_no' => $action_no,
+									'app_install_id' => $app_install_id,
+									'timestamp' => $timestamp);
+		$result = $this->stats->add_stat($data);
+		$result = $this->stats->add_stat($data);
+		$this->unit->run($result, 'is_true', 'add', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 100001);
+		$this->unit->run($result, 6, 'count', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 10001);
+		$this->unit->run($result, 3, 'count', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = 70;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 1001);
+		$this->unit->run($result, 1, 'count', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 60;
+		$campaign_id = NULL;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 1001);
+		$this->unit->run($result, 3, 'count', print_r($result, TRUE));
+		
+		$user_id = 100;
+		$action_no = 50;
+		$app_install_id = 70;
+		$campaign_id = NULL;
+		$result = $this->audit_stat_limit_lib->count($user_id, $action_no, $app_install_id, $campaign_id, 1001);
+		$this->unit->run($result, 2, 'count', print_r($result, TRUE));
+	}
+	
+}
+
+/* End of file audit_stat_limit_lib_test.php */
+/* Location: ./application/controllers/test/audit_stat_limit_lib_test.php */
---
+++ application/libraries/audit_stat_limit_lib.php
@@ -0,0 +1,105 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * MongoDB Audit Stat Limit Library
+ *
+ * A library to record audit stat to the NoSQL database MongoDB. 
+ *
+ * @author Metwara Narksook
+ */
+
+class Audit_stat_limit_lib
+{
+	
+	private $CI;
+	
+	/**
+	 *	--------------------------------------------------------------------------
+	 *	CONSTRUCTOR
+	 *	--------------------------------------------------------------------------
+	 *
+	 *	Automatically check if the Mongo PECL extension has been installed/enabled.
+	 * 
+	 * @author Metwara Narksook
+	 */
+	
+	function __construct(){
+		if(!class_exists('Mongo')){
+			show_error("The MongoDB PECL extension has not been installed or enabled",
+			 500);
+		}
+		$this->CI =& get_instance();
+	}
+	
+	/**
+	 * create index for all collection in database
+	 */
+	function create_index(){
+		$this->CI->load->model('audit_stats_model', 'stats');
+		$this->CI->stats->create_index();
+	}
+	
+	/**
+	 * add new audit stat entry to database
+	 * 
+	 * @param user_id (require)
+	 * @param action_no (require)
+	 * @param app_install_id (require)
+	 * @param campaign_id (optional)
+	 * 
+	 * @return result boolean
+	 * 
+	 * @author Metwara Narksook
+	 */
+	function add($user_id = NULL, $action_no = NULL, $app_install_id = NULL,
+	 $campaign_id = NULL){
+		$check_args = isset($user_id) && isset($action_no)
+		 && isset($app_install_id);
+		
+		if(!$check_args){
+			return FALSE;
+		}
+		
+		$data = array('user_id' => (int)$user_id,
+									'action_no' => (int)$action_no,
+									'app_install_id' => (int)$app_install_id);
+		if(isset($campaign_id)){
+			$data['campaign_id'] = (int)$campaign_id;
+		}
+		return $this->CI->stats->add_stat($data);
+	}
+	
+	/**
+	 * count stat in the past
+	 * @param user_id (require)
+	 * @param action_no (require)
+	 * @param app_install_id (require)
+	 * @param campaign_id (optional)
+	 * @param back_time_interval time in second for querying back 
+	 * 														(require - 1 hour default)
+	 * 
+	 * @return count (int)
+	 */
+	function count($user_id = NULL, $action_no = NULL, $app_install_id = NULL,
+	 $campaign_id = NULL, $back_time_interval = 3600){
+		date_default_timezone_set('Asia/Bangkok');
+		$now = time();
+		$criteria = array();
+		if(isset($user_id)){
+			$criteria['user_id'] = $user_id;
+		}
+		if(isset($action_no)){
+			$criteria['action_no'] = $action_no;
+		}
+		if(isset($app_install_id)){
+			$criteria['app_install_id'] = $app_install_id;
+		}
+		if(isset($campaign_id)){
+			$criteria['campaign_id'] = $campaign_id;
+		}
+		$criteria['timestamp'] = array('$gte' => $now - $back_time_interval);
+		return $this->CI->stats->count_stat($criteria);
+	}
+}
+/* End of file audit_stat_limit_lib.php */
+/* Location: ./application/libraries/audit_stat_limit_lib.php */
