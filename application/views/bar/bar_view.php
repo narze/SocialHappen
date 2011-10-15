a1edffc8e360d3a310599d61c3a61ce8ba2da446
NarzE <nazzarn@gmail.com>
2011-10-16 01:13:27 +0700

    Don't load node.js if not logged in (not neccessary)

--- application/views/bar/bar_view.php
+++ application/views/bar/bar_view.php
@@ -80,32 +80,34 @@
 		<?php endif; ?>
 	<?php endif; ?>
 </div>
-<script src="<?php echo $node_base_url;?>socket.io/socket.io.js"></script>
-<script>
-	(function($){
-	var session = 'SESSIONNAJA';
-	var socket = io.connect('<?php echo $node_base_url;?>');
+<?php if(!issetor($facebook_user)) : ?>
+	<script src="<?php echo $node_base_url;?>socket.io/socket.io.js"></script>
+	<script>
+		(function($){
+		var session = 'SESSIONNAJA';
+		var socket = io.connect('<?php echo $node_base_url;?>');
 
-	socket.on('connect', function(){
-		console.log('send subscribe');
-		socket.emit('subscribe', user_id, session);
-	});
+		socket.on('connect', function(){
+			console.log('send subscribe');
+			socket.emit('subscribe', user_id, session);
+		});
 
-	socket.on('subscribeResult', function (data) {
-		console.log('got subscribe result: ' + JSON.stringify(data));
-	});
+		socket.on('subscribeResult', function (data) {
+			console.log('got subscribe result: ' + JSON.stringify(data));
+		});
 
-	socket.on('newNotificationAmount', function (notification_amount) {
-		console.log('notification_amount: ' + notification_amount);
-		if(notification_amount > 0){
-			$('div.header ul.menu li.notification a.amount').html('').append('<span>').children('span').html(notification_amount);
-		}else{
-			$('div.header ul.menu li.notification a.amount').append('<span>').children('span').remove();
-		}
-	});
-	
-	socket.on('newNotificationMessage', function (notification_message) {
-		console.log('notification_message: ' + JSON.stringify(notification_message));
-	});
-	})(jQuery);
-</script>
+		socket.on('newNotificationAmount', function (notification_amount) {
+			console.log('notification_amount: ' + notification_amount);
+			if(notification_amount > 0){
+				$('div.header ul.menu li.notification a.amount').html('').append('<span>').children('span').html(notification_amount);
+			}else{
+				$('div.header ul.menu li.notification a.amount').append('<span>').children('span').remove();
+			}
+		});
+		
+		socket.on('newNotificationMessage', function (notification_message) {
+			console.log('notification_message: ' + JSON.stringify(notification_message));
+		});
+		})(jQuery);
+	</script>
+<?php endif; ?>
