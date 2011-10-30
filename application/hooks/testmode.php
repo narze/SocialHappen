b9d00cf83b19622fcb720377387bba5dd4b995ee
Noom <noom@figabyte.com>
2011-10-30 22:40:17 +0700

    xd class should get through test mode

--- application/hooks/testmode.php
+++ application/hooks/testmode.php
@@ -8,7 +8,8 @@ class TestMode {
 			if(!in_array($_SERVER['REMOTE_ADDR'], array('216.113.188.202','216.113.188.203','216.113.188.204','66.211.170.66','173.0.82.126'))){ //Skip if from paypal
 				if(isset($_SERVER['HTTP_USER_AGENT']) //letting calls from file_get_contents() bypass
 				&& (empty($_SERVER['HTTP_X_REQUESTED_WITH']) || strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) != 'xmlhttprequest')){ //letting calls from ajax bypass
-					if($ci->router->fetch_class() != 'tab'){
+					$excepted_classes = array('tab','xd');
+					if(!in_array($ci->router->fetch_class(),$excepted_classes)){
 						$ci->load->library('session');
 						if(!$ci->session->userdata('test_mode')){
 							$password = $ci->input->post('test_mode_password');
