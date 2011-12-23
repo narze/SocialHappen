0ee8baf54d3ec8ebbac1b34da6915eed4d24c848
Noom <noom@figabyte.com>
2011-12-23 17:12:02 +0700

    update beta facebook config

--- application/config/production/facebook.php
+++ application/config/production/facebook.php
@@ -6,3 +6,4 @@
 	$config['facebook_admin_scope']		= 'user_about_me,email,publish_stream,manage_pages';
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
 	$config['facebook_callback_url'] 	= 'socialhappen'; // relate with site_url();
+	$config['facebook_app_canvas_url']		= 'https://apps.facebook.com/';
