58de77a0c6812ef1b463004e1865999bac07818f
Noom <noom@figabyte.com>
2011-12-29 17:34:41 +0700

    Added bar_ctrl and test

--- application/controllers/bar.php
+++ application/controllers/bar.php
@@ -5,6 +5,7 @@ class Bar extends CI_Controller {
 	function __construct(){
 		parent::__construct();
 		$this->load->library('form_validation');
+		$this->load->library('controller/bar_ctrl');
 	}
 
 	/**
@@ -25,34 +26,18 @@ class Bar extends CI_Controller {
 		}
 		else 
 		{
-			$company_image = $this->socialhappen->upload_image('company_image');
-		 	$company = array(
-					       	'company_name' => set_value('company_name'),
-					       	'company_detail' => set_value('company_detail'),
-					       	'company_image' => !$company_image ? base_url().'assets/images/default/company.png' : $company_image,
-					       	'creator_user_id' => $this->socialhappen->get_user_id()
-						);
-			$this->load->model('company_model', 'companies');
-			$company_id = $this->companies->add_company($company);
-			
-			if ($company_id) 
-			{
-				$this->load->model('user_companies_model','user_companies');
-				$user_company = array(
-						'user_id' => $this->socialhappen->get_user_id(),
-						'company_id' => $company_id,
-						'user_role' => 1 //Company Admin
-					);
-				if($result = $this->user_companies->add_user_company($user_company)){
-					$this->load->view('common/redirect',array('redirect_parent'=>base_url().'company/'.$company_id));
-				} else {
-					echo "Error adding user company";
-				}
-			}
-			else
-			{
-				log_message('error','company add failed');
-				echo 'Error adding company';
+			$user_id = $this->socialhappen->get_user_id();
+			$company_name = set_value('company_name');
+			$company_detail = set_value('company_detail');
+    	 	$company_image = $this->socialhappen->upload_image('company_image');
+			$input = compact('user_id', 'company_name', 'company_detail' ,'company_image');
+			$result = $this->bar_ctrl->create_company($input);
+
+			if($result['success']){
+				$company_id = $result['data']['company_id'];
+				$this->load->view('common/redirect',array('redirect_parent'=>base_url().'company/'.$company_id));
+			} else {
+				echo $result['error'];
 			}
 		}
 	}
@@ -63,30 +48,14 @@ class Bar extends CI_Controller {
 	 */
 	function select_company(){
 		$this -> socialhappen -> check_logged_in();
-		$user = $this->socialhappen->get_user();
+		$user_id = $this->socialhappen->get_user_id();
 		
-		$this->load->model('user_companies_model','user_companies');
-		$user_companies = $this->user_companies->get_user_companies_by_user_id($user['user_id']);
-		
-		if($user_companies)
-		{
-			$this->load->model('page_model','page');
-			$this->load->model('installed_apps_model','installed_app');
-			$this->load->model('campaign_model','campaigns');
-			foreach($user_companies as &$company) 
-			{
-				$company['page_count'] = $this->page->count_all(array("company_id" => $company['company_id']));
-				$company['app_count'] = $this->installed_app->count_all_distinct("app_id",array("company_id" => $company['company_id']));
-				$company['campaign_count'] = $this->campaigns->count_campaigns_by_company_id($company['company_id']);
-			}
+		$result = $this->bar_ctrl->select_company($user_id);
+		if($result['success']){
+			$this->parser->parse('bar/select_company_view', $result['data']);
+		} else {
+			echo $result['error'];
 		}
-		
-		$data = array(
-			'user_companies' => $user_companies,
-			'user_can_create_company' => $this->socialhappen->check_package_by_user_id_and_mode($user['user_id'], 'company')  //Check user can create company
-		);
-		$this->parser->parse('bar/select_company_view', $data);
-		return $data;
 	}
 }  
 
---
+++ application/libraries/controller/bar_ctrl.php
@@ -0,0 +1,85 @@
+<?php  if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+class Bar_ctrl {
+
+	private $CI;
+
+	function __construct() {
+        $this->CI =& get_instance();
+    }
+
+    function create_company($input = array())
+    {
+    	$result = array('success' => FALSE);
+    	if(!$user_id = issetor($input['user_id'])){
+    		$result['error'] = 'No user_id specified';
+    	} else {
+    		if(!isset($input['company_name']) || !isset($input['company_detail']) || !isset($input['company_image'])){
+    			$result['error'] = 'Insufficient data';
+    		} else {
+			 	$company = array(
+						       	'company_name' => $input['company_name'],
+						       	'company_detail' => $input['company_detail'],
+						       	'company_image' => !$input['company_image'] ? base_url().'assets/images/default/company.png' : $input['company_image'],
+						       	'creator_user_id' => $user_id
+							);
+				$this->CI->load->model('company_model', 'companies');
+				
+				
+				if ($company_id = $this->CI->companies->add_company($company)) 
+				{
+					$this->CI->load->model('user_companies_model','user_companies');
+					$user_company = array(
+							'user_id' => $user_id,
+							'company_id' => $company_id,
+							'user_role' => 1 //Company Admin
+						);
+					if($this->CI->user_companies->add_user_company($user_company)){
+						$result['success'] = TRUE;
+						$result['data'] = array(
+							'company_id' => $company_id
+						);
+					} else {
+						$result['error'] = "Error adding user company";
+					}
+				}
+				else
+				{
+					// log_message('error','company add failed');
+					$result['error'] = 'Error adding company';
+				}
+			}
+		}
+		return $result;
+    }
+
+    function select_company($user_id = NULL){
+    	$result = array('success' => FALSE);
+    	if(!$user_id){
+    		$result['error'] = 'No user_id specified';
+    	} else {
+	    	$this->CI->load->model('user_companies_model','user_companies');
+			$user_companies = $this->CI->user_companies->get_user_companies_by_user_id($user_id);
+			
+			if($user_companies)
+			{
+				$this->CI->load->model('page_model','page');
+				$this->CI->load->model('installed_apps_model','installed_app');
+				$this->CI->load->model('campaign_model','campaigns');
+				foreach($user_companies as &$company) 
+				{
+					$company['page_count'] = $this->CI->page->count_all(array("company_id" => $company['company_id']));
+					$company['app_count'] = $this->CI->installed_app->count_all_distinct("app_id",array("company_id" => $company['company_id']));
+					$company['campaign_count'] = $this->CI->campaigns->count_campaigns_by_company_id($company['company_id']);
+				}
+			}
+			
+			$result['data'] = array(
+				'user_companies' => $user_companies,
+				'user_can_create_company' => $this->CI->socialhappen->check_package_by_user_id_and_mode($user_id, 'company')  //Check user can create company
+			);
+			$result['success'] = TRUE;
+		}
+		return $result;
+    }
+}
