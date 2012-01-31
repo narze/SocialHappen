1ad974345796ec6765d8f43badbaa2ddb508bf7e
Noom <noom@figabyte.com>
2012-01-31 13:42:49 +0700

    Moved terms & conditions

--- application/views/settings/page_apps/reward.php
+++ application/views/settings/page_apps/reward.php
@@ -1,16 +1,5 @@
 	<div id="reward">
 		<h2><span>Reward List Setting</span></h2>
-
-		<?php echo form_open("settings/page_reward/view/{$page_id}", array('class' => 'reward')); 
-		if($updated) echo '<div class="notice success">Updated</div>'; ?>
-		<h3>Terms and Conditions</h3>
-		<?php echo form_error('terms_and_conditions'); ?>
-		<br />
-		<?php echo form_textarea( array( 'name' => 'terms_and_conditions', 'value' => set_value('terms_and_conditions', $terms_and_conditions) ) )?>
-
-		<?php echo form_submit('submitForm', 'Submit', 'class="bt-update"');
-	    echo form_close(); ?>
-
 		<div class="tab-head">
 			Sort by :
 			<a class="tab active">Date Added</a>
