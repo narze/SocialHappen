b7d7a35bc764e7240ebf9ef6a869c744c079080d
NarzE <manassarn@gmail.com>
2012-02-13 13:28:48 +0700

    Added facebook_app_id & facebook_app_secret for production server

--- application/config/production/facebook.php
+++ application/config/production/facebook.php
@@ -1,7 +1,7 @@
 <?php
 	//socialhappen.com
-	$config['facebook_app_id'] 			= '';
-	$config['facebook_api_secret'] 		= '';
+	$config['facebook_app_id'] 			= '383532658328333';
+	$config['facebook_api_secret'] 		= '4a674a85f3b7f2e8c24d5a8633b0999b';
 	$config['facebook_default_scope']	= 'user_about_me,email,publish_stream,offline_access';
 	$config['facebook_admin_scope']		= 'user_about_me,email,publish_stream,manage_pages,offline_access';
 	$config['facebook_api_url'] 		= 'https://graph.facebook.com/'; // Just in case it changes.
