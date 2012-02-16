833a5c33eaa9cf1bf3fc315bd684282b146a0695
NarzE <manassarn@gmail.com>
2012-02-16 16:10:52 +0700

    Using mongodump.sh should prompt username,password and mongodb port

--- shdumper/mongodump.sh
+++ shdumper/mongodump.sh
@@ -10,8 +10,12 @@ LOG_FILE=$LOGS_LOCATION/log-mongodump-`date -u +%Y%m%d_%H%M%S`
 DIR_NAME_ABS=$DUMP_LOCATION/$DIR_NAME
 COPY_SSH_DEST=server1:$DIR_NAME_ABS
 # Uncomment and configure
-USERNAME="sohap"
-PASSWORD="figyfigy"
+echo "u"
+read USERNAME
+echo "p"
+read PASSWORD
+echo ": (27017)"
+read PORT
 ##--
 
 AUTH=""
@@ -25,7 +29,7 @@ echo "Starting backup `date`" >> $LOG_FILE
 for DB in $DBS
 do
 	echo "Backup: $DB" >> $LOG_FILE
-	mongodump --db $DB $AUTH --out $DIR_NAME_ABS >> $LOG_FILE
+	mongodump --db $DB --port $PORT $AUTH --out $DIR_NAME_ABS >> $LOG_FILE
 #	tar -czf $DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz --remove-files --directory $DUMP_LOCATION $DIR_NAME >> $LOG_FILE
 #	scp	$DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz $COPY_SSH_DEST >> $LOG_FILE
 done
