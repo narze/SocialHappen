ac6a20b2e581b52948f9057f93b4ca1808ba9ee6
NarzE <manassarn@gmail.com>
2012-02-21 11:28:14 +0700

    Trivial fix

--- application/libraries/controller/tab_ctrl.php
+++ application/libraries/controller/tab_ctrl.php
@@ -285,8 +285,7 @@ class Tab_ctrl {
 			$this->CI->load->model('invite_pending_model', 'invite_pending');
 			if($invite_key = $this->CI->invite_pending->get_invite_key_by_user_facebook_id_and_campaign_id($user_facebook_id, $campaign_id)){
 				$this->CI->load->library('invite_component_lib');
-				$accept_invite_result = $this->CI->invite_component_lib->accept_invite_campaign_level($invite_key, $user_facebook_id);
-				if(!$accept_invite_result){
+				if(!$accept_invite_result = $this->CI->invite_component_lib->accept_invite_campaign_level($invite_key, $user_facebook_id)){
 					$data = array(
 						'status' => 'error',
 						'error' => 'accept_invite_error',
