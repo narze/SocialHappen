bc20105a8ab72a4983d729ceb3d821a7a55f8858
NarzE <manassarn@gmail.com>
2012-04-19 14:46:24 +0700

    Change production database port to 27017

--- application/config/production/mongo_db.php
+++ application/config/production/mongo_db.php
@@ -1,9 +1,9 @@
 <?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
 $config['mongo_host'] = 'localhost';
-$config['mongo_port'] = 27027;
+$config['mongo_port'] = 27017;
 $config['mongo_db'] = 'socialhappen';
-$config['mongo_user'] = 'sohappen';
+$config['mongo_user'] = 'sohap';
 $config['mongo_pass'] = 'figyfigy';
 $config['mongo_persist'] = TRUE;
 $config['mongo_persist_key'] = 'ci_mongo_persist';
