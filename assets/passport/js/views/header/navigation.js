a08b9ffd20166b362caac0f8023e51f718f5da30
Metwara Narksook <hybridknight@gmail.com>
2012-05-10 12:27:36 -0700

    add bar notification in passport page

--- assets/passport/js/views/header/navigation.js
+++ assets/passport/js/views/header/navigation.js
@@ -3,10 +3,12 @@ define([
   'underscore',
   'backbone',
   'bootstrap',
-  'text!templates/header/navigation.html' 
-], function($, _, Backbone, bootstrap, headerMenuTemplate){
+  'text!templates/header/navigation.html',
+  'text!templates/header/bar-notification.html'
+], function($, _, Backbone, bootstrap, headerMenuTemplate, barNotificationTemplate){
   var HeaderNavigationView = Backbone.View.extend({
     headerMenuTemplate: _.template(headerMenuTemplate),
+    barNotificationTemplate: _.template(barNotificationTemplate),
     el: '#header',
     events: {
 
@@ -21,6 +23,27 @@ define([
         user: this.options.currentUserModel.toJSON()
       }));
       $('div#header .passport').addClass('active');
+      
+      var barNotificationTemplate = this.barNotificationTemplate;
+      
+      $.ajax({
+        url: window.Passport.BASE_URL + 'apiv3/notifications/' + window.Passport.userId,
+        type: "POST",
+        dataType: "json",
+        success: function(data) {
+          console.log('notification', data, $('.bar-notification'));
+
+          $('.bar-notification').replaceWith(barNotificationTemplate({
+            baseUrl: window.Passport.BASE_URL,
+            notifications: {
+              count: data.count,
+              list: data.items
+            }
+          }));
+          $('.no-notification').hide();
+        }
+      });
+      
       return this;
     }
   })
