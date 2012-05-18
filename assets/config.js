142bbf83fd7e7337bc52cc3a82f6c61f3d16b366
Metwara Narksook <hybridknight@gmail.com>
2012-05-18 07:34:38 -0700

    add client config in assets/config.js

---
+++ assets/config.js
@@ -0,0 +1,11 @@
+// var BASE_URL = 'https://beta.socialhappen.com/';
+var BASE_URL = 'https://socialhappen.dyndns.org/socialhappen/';
+
+window.Passport = {
+  BASE_URL: BASE_URL
+}
+
+window.World = {
+  BASE_URL: BASE_URL
+}
+
