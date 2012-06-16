b1c2085bfbd782308c030b03e90c9d8bc5ec78b6
Metwara Narksook <hybridknight@gmail.com>
2012-06-16 16:05:04 +0700

    show challenge status in world page

--- assets/world/js/views/world/challenge-item.js
+++ assets/world/js/views/world/challenge-item.js
@@ -14,6 +14,12 @@ define([
     render: function () {
       var data = this.model.toJSON();
       data.baseUrl = window.World.BASE_URL;
+      var now = Math.floor(new Date().getTime()/1000);
+      console.log(data.detail.name, data.start_date, data.end_date, now);
+      
+      data.expired = data.end_date < now;
+      data.notstart = data.start_date > now;
+      
       $(this.el).html(this.challengeItemTemplate(data));
       return this;
     }
