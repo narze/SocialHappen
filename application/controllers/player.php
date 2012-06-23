ba5ecb5832d4c02a5f27c2dc5163f8cf97dcaf1c
Manassarn Manoonchai <manassarn@gmail.com>
2012-06-23 18:44:50 +0700

    Fix misplaced code

--- application/controllers/player.php
+++ application/controllers/player.php
@@ -156,18 +156,19 @@ class Player extends CI_Controller {
         $player_challenging = FALSE;
         $now = date('Ymd');
         if(isset($user['daily_challenge'][$challenge_id])) {
+          //Get last challenge attempt
           $challenge_range = array_pop($user['daily_challenge'][$challenge_id]);
           if($challenge_range['start_date'] <= $now && $now <= $challenge_range['end_date']) {
             $player_challenging = TRUE;
           }
-        }
 
-        $date = $challenge_range['end_date'].'';
-        $y = substr($date, 0, 4);
-        $m = substr($date, 4, 2);
-        $d = substr($date, 6, 2);
-        date_default_timezone_set('UTC');
-        $challenge_available_date = date('Y-m-d', mktime(0, 0, 0, $m, $d + 1, $y));
+          $date = $challenge_range['end_date'].'';
+          $y = substr($date, 0, 4);
+          $m = substr($date, 4, 2);
+          $d = substr($date, 6, 2);
+          date_default_timezone_set('UTC');
+          $challenge_available_date = date('Y-m-d', mktime(0, 0, 0, $m, $d + 1, $y));
+        }
       }
 
       $action_done = $this->input->get('action_done') && $user_id;
