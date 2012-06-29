b09b1dd58dd075d4f7c5a298066106fe23cf39a5
Manassarn Manoonchai <manassarn@gmail.com>
2012-06-29 17:44:23 +0700

    Coupon item modal

--- assets/passport/js/views/profile/coupon-item.js
+++ assets/passport/js/views/profile/coupon-item.js
@@ -2,13 +2,16 @@ define([
   'jquery',
   'underscore',
   'backbone',
-  'text!templates/profile/coupon-item.html'
-], function($, _, Backbone, couponItemTemplate){
+  'text!templates/profile/coupon-item.html',
+  'text!templates/profile/reward-item-modal.html'
+], function($, _, Backbone, couponItemTemplate, rewardItemTemplate){
   var CouponItemView = Backbone.View.extend({
     tagName: 'div',
     className: 'item',
     couponItemTemplate: _.template(couponItemTemplate),
+    rewardItemTemplate: _.template(rewardItemTemplate),
     events: {
+      'click button.view-reward': 'viewReward'
     },
     initialize: function(){
       _.bindAll(this);
@@ -18,9 +21,23 @@ define([
     render: function () {
       console.log('render coupon item');
       var data = this.model.toJSON();
-      data.baseUrl = window.Company.BASE_URL;
+      data.baseUrl = window.Passport.BASE_URL;
       $(this.el).html(this.couponItemTemplate(data));
       return this;
+    },
+    viewReward: function() {
+      var reward_item_id = this.model.get('reward_item_id');
+      var self = this;
+      $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: window.Passport.BASE_URL + 'apiv3/reward_item/' + reward_item_id,
+        success: function(res) {
+          var data = res.data;
+          data.coupon = self.model.toJSON();
+          $('#reward-item').html(self.rewardItemTemplate(data)).modal();
+        }
+      });
     }
   });
   return CouponItemView;
