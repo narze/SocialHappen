df72ce13e669991ec220c1f65f583aafc9092e4b
hybridknight <hybridknight@gmail.com>
2012-07-28 14:21:58 +0700

    add default text

--- assets/company/js/views/company/coupon-list.js
+++ assets/company/js/views/company/coupon-list.js
@@ -53,6 +53,11 @@ define([
 
     addAll: function(){
       $('.coupon-list', this.el).html('');
+
+      if(this.collection.models.length == 0){
+        $('.coupon-list', this.el).html('Your company have no coupon.');
+      }
+
       this.collection.each(function(model){
         this.addOne(model);
       }, this);
