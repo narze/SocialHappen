6cc077b23b88c0e2491a3301f7f2e2c007bb4acb
Manassarn Manoonchai <manassarn@gmail.com>
2012-07-30 14:55:25 +0700

    Improved mongodb_sync & don't permit beta server (testing ENV) to reset

--- application/controllers/dev/sync.php
+++ application/controllers/dev/sync.php
@@ -9,7 +9,7 @@ class Sync extends CI_Controller {
 	function __construct(){
 		if (defined('ENVIRONMENT'))
 		{
-			if (!(ENVIRONMENT == 'development' || ENVIRONMENT == 'testing' ))
+			if (!(ENVIRONMENT == 'development'))
 			{
 				redirect();
 			}
@@ -27,7 +27,7 @@ class Sync extends CI_Controller {
 	function index(){
 		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/db_reset">[Production] Mysql reset</a><br />';
 		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/mongodb_reset">[Production] MongoDB reset</a><br />';
-		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/db_reset?unit_test=1">[Unit test] Mysql reset<br />';
+		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/db_reset?unit_test=1">[Unit test] Mysql reset</a><br />';
 		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/mongodb_reset?unit_test=1">[Unit test] MongoDB reset</a><br />';
 		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/remove_users">Remove users</a><br />';
 		echo '<a target="_blank" href="'.BASE_URL.'dev/sync/generate_field_code">Generate field PHP code</a><br />';
@@ -51,44 +51,19 @@ class Sync extends CI_Controller {
 	}
 
 	function mongodb_reset($skip = FALSE){
-		//Check if mongodb is backed up manually in last x=$interval_limit seconds
+		$timestamp = date('Ymd_His');
+		$output = array();
+		$ret = null;
+		echo $this->config->item('mongo_user');
+		exec("cd ". FCPATH. "shdumper && sh mongodump.sh sohap figyfigy 27017 {$timestamp}", $output, $ret);
+		$backup_success = file_exists(FCPATH . "application/backup/sh_mongo-{$timestamp}");
 
-		//1.Get latest backup file name
-		$prefix = 'sh_mongo\-';
-		$interval_limit = 120;
-		$backup_dir = glob(APPPATH.'backup/sh_mongo-*_*',GLOB_ONLYDIR);
-		$file_count = count($backup_dir);
-		// var_dump_pre($backup_dir);
-		for ($i = 0; $i < $file_count; $i++)
-		{
-		// Mark wrongly formatted files as false for later filtering
-		$name = basename($backup_dir[$i], '.php');
-			if ( ! preg_match('/^'.$prefix.'\d{8}_\d{6}$/', $name))
-			{
-				$backup_dir[$i] = FALSE;
-			}
+		if($backup_success) {
+			echo 'Backup completed<br />';
+			return $this->db_sync->mongodb_reset();
 		}
 
-		sort($backup_dir);
-		$latest_backup = basename(end($backup_dir));
-
-		//2.try convert into unit timestamp
-		$latest_backup = preg_replace('/^('.$prefix.')(\w+)_(\w+)/','${2}${3}',$latest_backup);
-		$y = (int) substr($latest_backup,0,4);
-		$m = (int) substr($latest_backup,4,2);
-		$d = (int) substr($latest_backup,6,2);
-		$h = (int) substr($latest_backup,8,2);
-		$i = (int) substr($latest_backup,10,2);
-		$s = (int) substr($latest_backup,12,2);
-		$latest_backup_timestamp = mktime($h, $i, $s, $m, $d, $y);
-
-		//3.compare with time()
-		$interval = time()-$latest_backup_timestamp;
-		if($interval <= $interval_limit || ($skip == date('md'))) {
-			$this->db_sync->mongodb_reset();
-		} else {
-			echo 'Please try backup using mongodump before reset, or append date (MMDD) to skip this';
-		}
+		echo 'Backup failed, no reset';
 	}
 
 	function generate_field_code(){
--- shdumper/mongodump.sh
+++ shdumper/mongodump.sh
@@ -2,21 +2,19 @@
 
 #--
 # CONFIG
-DBS="socialhappen_beta socialhappen"
-DUMP_LOCATION=../application/backup
-LOGS_LOCATION=../application/backup
-DIR_NAME=sh_mongo-`date -u +%Y%m%d_%H%M%S`
-LOG_FILE=$LOGS_LOCATION/log-mongodump-`date -u +%Y%m%d_%H%M%S`
+USERNAME=$1
+PASSWORD=$2
+PORT=$3
+TIMESTAMP=$4
+
+PHYS_DIR=`pwd -P`
+DBS="socialhappen socialhappen_beta"
+DUMP_LOCATION=$PHYS_DIR/../application/backup
+LOGS_LOCATION=$PHYS_DIR/../application/backup
+DIR_NAME=sh_mongo-$TIMESTAMP
+LOG_FILE=$LOGS_LOCATION/log-mongodump-$TIMESTAMP
 DIR_NAME_ABS=$DUMP_LOCATION/$DIR_NAME
 COPY_SSH_DEST=server1:$DIR_NAME_ABS
-# Uncomment and configure
-echo "u"
-read USERNAME
-echo "p"
-read PASSWORD
-echo ": (27017)"
-read PORT
-##--
 
 AUTH=""
 
@@ -29,7 +27,8 @@ echo "Starting backup `date`" >> $LOG_FILE
 for DB in $DBS
 do
 	echo "Backup: $DB" >> $LOG_FILE
-	mongodump --db $DB --port $PORT $AUTH --out $DIR_NAME_ABS >> $LOG_FILE
+  echo "Command : mongodump --db $DB --port $PORT $AUTH --out $DIR_NAME_ABS >> $LOG_FILE"
+	./mongodump --db $DB --port $PORT $AUTH --out $DIR_NAME_ABS
 #	tar -czf $DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz --remove-files --directory $DUMP_LOCATION $DIR_NAME >> $LOG_FILE
 #	scp	$DUMP_LOCATION/$DB-`date +%Y%m%d`.tgz $COPY_SSH_DEST >> $LOG_FILE
 done
