da5e809c66273de851cd6cb0f78c673262c33242
hybridknight <hybridknight@gmail.com>
2012-08-04 15:00:47 +0700

    add more stamp info and link to coupon

--- assets/passport/js/views/profile/card-item.js
+++ assets/passport/js/views/profile/card-item.js
@@ -2,13 +2,16 @@ define([
   'jquery',
   'underscore',
   'backbone',
+  'bootstrap',
   'text!templates/profile/card-item.html',
   'text!templates/profile/card-activity-item.html'
-], function($, _, Backbone, cardItemTemplate, cardActivityItemTemplate){
+], function($, _, Backbone, bootstrap, cardItemTemplate, cardActivityItemTemplate){
   var CardItemView = Backbone.View.extend({
     className: 'card',
 
-    events: {},
+    events: {
+      'click a.coupon': 'onClickCoupon'
+    },
 
     initialize: function(){
       _.bindAll(this);
@@ -27,11 +30,20 @@ define([
     addAllActivities: function(activities) {
       $('.card-activity-list', this.el).empty();
       _.each(activities, this.addOneActivity);
+      this.$('a.coupon').popover({
+        placement: 'top'
+      });
     },
 
     addOneActivity: function(activity) {
       console.log(activity);
+      activity.reward_item.reward_item_id = activity.reward_item_id;
+      activity.reward_item.time = moment.unix(activity.timestamp).fromNow();
       $('.card-activity-list', this.el).append(_.template(cardActivityItemTemplate)(activity.reward_item));
+    },
+
+    onClickCoupon: function(){
+      this.$('a.coupon').popover('hide');
     }
 
   });
