bc254dfb557673601753f0a1ef8b498f07f700f1
Manassarn Manoonchai <manassarn@gmail.com>
2012-10-16 17:22:36 +0700

    add APN library and test

--- application/controllers/apiv4.php
+++ application/controllers/apiv4.php
@@ -1270,4 +1270,57 @@ class Apiv4 extends REST_Controller {
 
     return $this->success($coupons);
   }
+
+  /**
+   * Test Push Notification
+   */
+  function test_push_notification_get()
+  {
+      //@TODO get everyone's device token
+      //narze's
+      $device_token = 'e13b8f33d312bcb227cd3260e46e207b7a01d0f331b58f39cc2e91cc35cd6978';
+
+      $this->load->library('apn');
+      $this->apn->payloadMethod = 'enhance'; // you can turn on this method for debuggin purpose
+      $this->apn->connectToPush();
+
+      // adding custom variables to the notification
+      $this->apn->setData(array( 'someKey' => true ));
+
+      $send_result = $this->apn->sendMessage($device_token, 'Test notif #1 (TIME:'.date('H:i:s').')', /*badge*/ 2, /*sound*/ 'default'  );
+
+      if($send_result) {
+        echo 'sended';
+          // log_message('debug','Sending successful');
+
+      }
+      else {
+          log_message('error',$this->apn->error);
+      }
+
+      $this->apn->disconnectPush();
+  }
+
+  // designed for retreiving devices, on which app not installed anymore
+  public function apn_feedback()
+  {
+      $this->load->library('apn');
+
+      $unactive = $this->apn->getFeedbackTokens();
+
+      if (!count($unactive))
+      {
+          log_message('info','Feedback: No devices found. Stopping.');
+          return false;
+      }
+
+      foreach($unactive as $u)
+      {
+          $devices_tokens[] = $u['devtoken'];
+      }
+
+      /*
+      print_r($unactive) -> Array ( [0] => Array ( [timestamp] => 1340270617 [length] => 32 [devtoken] => 002bdf9985984f0b774e78f256eb6e6c6e5c576d3a0c8f1fd8ef9eb2c4499cb4 ) )
+      */
+  }
 }
