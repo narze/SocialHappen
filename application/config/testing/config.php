0bf5163227157f1ef34f9dd58bd5745062b438bd
Manassarn Manoonchai <manassarn@gmail.com>
2012-10-20 20:01:40 +0700

    change beta's base_url

--- application/config/testing/config.php
+++ application/config/testing/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://shbeta.thaimeme.com/';
+$config['base_url']	= 'http://shbeta.thaimeme.com/socialhappen/';
 
 /**
  * Node.js server base url
