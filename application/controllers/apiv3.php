6d1840618ddf9e9dfbd14181b5fc7dd2c9f2f5a7
hybridknight <hybridknight@gmail.com>
2012-12-22 13:21:04 +0700

    can add branch

--- application/controllers/apiv3.php
+++ application/controllers/apiv3.php
@@ -31,6 +31,50 @@ class Apiv3 extends CI_Controller {
   }
 
   /**
+   * create/update branch
+   */
+  function saveBranch($branch_id = NULL){
+    header('Content-Type: application/json', TRUE);
+    if(!$user = $this->socialhappen->get_user()) {
+      return json_return(array('success' => FALSE, 'data' => 'Not signed in'));
+    }
+
+    $branch = rawurldecode($this->input->post('model', TRUE));
+
+    if(!isset($branch) || $branch == ''){
+      return json_return(array('success' => FALSE, 'data' => 'no branch data'));
+    }
+
+    $this->load->library('branch_lib');
+    $branch = json_decode($branch, TRUE);
+
+    if(!is_array($branch)){
+      return json_return(array('success' => FALSE, 'data' =>'data error'));
+      return FALSE;
+    }
+
+    $branch['updated_timestamp'] = time();
+
+    if(isset($branch['_id'])) {
+      //Branch exists : update
+      $branch_item_id = $branch['_id'];
+      $criteria = array('_id' => new MongoId($branch_item_id));
+
+      if(!$branch_update_result = $this->branch_lib->update($criteria, $branch)) {
+        return json_return(array('success' => FALSE, 'data' => 'Update branch failed'));
+      }
+    } else {
+      //New branch : add new
+      if(!$branch = $this->branch_lib->add($branch)) {
+        return json_return(array('success' => FALSE, 'data' => 'Add branch failed'));
+      }
+    }
+
+    return json_return(array('success' => TRUE, 'data' => $branch));
+
+  }
+
+  /**
    * get platform user
    *
    * @param user_id [required]
