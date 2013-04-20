8b5e8e8b87650577497d5be604134f76dad171ee
Weerapat Poosri <vinboxx@gmail.com>
2013-04-20 14:48:58 +0700

    Fix gashapon layout

--- application/views/video/gashapon_view.php
+++ application/views/video/gashapon_view.php
@@ -5,19 +5,19 @@
     <!-- <meta name="viewport" content="width=1024" /> -->
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <title>SocialHappen - How to play</title>
-    <link rel="stylesheet" href="assets/css/common/bootstrap.min.css">
-    <link rel="stylesheet" href="assets/css/common/fancybox/jquery.fancybox-2.1.4.css">
-    <link rel="stylesheet" href="assets/css/video/gashapon.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/common/bootstrap.min.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/common/fancybox/jquery.fancybox-2.1.4.css">
+    <link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/video/gashapon.css">
 </head>
 <body>
     <header class="navbar navbar-fixed-top" role="banner" id="navbar">
         <div class="navbar-inner">
                 <a title="Home" href="/" class="brand">
-                    <img height="50" alt="Home" class="hires" src="assets/images/howto/party-logo2.png">
+                    <img height="50" alt="Home" class="hires" src="<?php echo base_url(); ?>assets/images/howto/party-logo2.png">
                 </a>
                 <div class="bot-nav">
                     <ul>
-                        <li><a href="#/step-1" id="step-1" class="active">Home</a></li>
+                        <li><a href="#/step-1" id="step-1">Home</a></li>
                         <li><a href="#/step-4" id="step-4">Why Socialhappen?</a></li>
                         <li><a href="#/step-5" id="step-5">How it works</a></li>
                         <li><a href="#/step-2" id="step-2">Technology</a></li>
@@ -38,12 +38,12 @@
             <div class="container">
                 <div class="row-fluid">
                     <div class="span7 play-vdo">
-                        <img src="assets/images/howto/video-01.png" alt="">
+                        <img src="<?php echo base_url(); ?>assets/images/howto/video-01.png" alt="">
                         <div class="btn-play" href="http://www.youtube.com/embed/AqA2_DzQDBQ?rel=0&autoplay=1&hd=1&vq=hd720"></div>
                     </div>
                     <div class="span5">
                         <h1>SocialHappen</h1>
-                        <p class="subheading">SocialHappen helps retails drive real foot traffic, and better engage with customers.</p>
+                        <p class="subheading">SocialHappen helps retails <br>drive real foot traffic, and better <br>engage with customers.</p>
                         <a class="btn btn-primary btn-large" href="#/step-2">Tell me more</a>
                     </div>
                 </div>
@@ -51,15 +51,15 @@
         </div>
 
         <!-- Slide 02 -->
-        <div class="step slide" data-x="2500" data-y="0" data-z="-200" data-rotate="90">
-            <h1>What will happen if objects can talk to smartphone?</h1>
+        <div class="step slide" data-x="2455" data-y="0" data-rotate="90">
+            <h1 style="padding-top: 108px;">What will happen if objects <br>can talk to smartphone?</h1>
             <p class="subheading">SocialHappen Technology enables physical business connect to their customers in an engaging way.</p>
             <p class="subheading">We build a ‘retail marketing platform’ that allow real business to connect and engage with their customers when they visit the store. We motivate your customers to come more often, and turn them to be an influencers via Socialhappen’s rewarding system.</p>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span4 align-center"><img src="assets/images/howto/sh-slide-01.png" alt=""></div>
-                    <div class="span4 align-center"><img src="assets/images/howto/sh-slide-02.png" alt=""></div>
-                    <div class="span4 align-center"><img src="assets/images/howto/sh-slide-03.png" alt=""></div>
+                    <div class="span4 align-center"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-01.png" alt=""></div>
+                    <div class="span4 align-center"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-02.png" alt=""></div>
+                    <div class="span4 align-center"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-03.png" alt=""></div>
                 </div>
                 <div class="row-fluid">
                     <div class="span4 align-center"><p class="subheading">Objects with<br> SocialHappen inside</p></div>
@@ -71,39 +71,39 @@
         </div>
 
         <!-- Slide 03 -->
-        <div class="step slide" data-x="3000" data-y="2000" data-z="-300" data-rotate="135">
-            <h1>Connect your offline business to online presence easily</h1>
+        <div class="step slide" data-x="2455" data-y="1500" data-rotate="90">
+            <h1 style="padding-top: 128px;">Connect your offline business to online presence easily</h1>
             <p class="subheading">This magic can happen because we build our own <span class="underline">transmission technology</span> that can communicate to the app users when they walk in to the store. The magic box will instantly send the challenge to them, let them engage with the marketing actions, and return some rewards when they complete it. Unlike QR code that users rarely scan, Socialhappen technology allow business to connect and engage with their customers easily.</p>
             <div class="container">
                 <div class="row-fluid">
-                    <img src="assets/images/howto/sh-slide-16.png" alt="">
+                    <img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-16.png" alt="">
                 </div>
             </div>
         </div>
 
         <!-- Slide 04 -->
-        <div class="step slide" data-x="1000" data-y="3000" data-z="500" data-rotate="180">
+        <div class="step slide" data-x="0" data-y="3000" data-rotate="90">
             <p class="subheading">What can Socialhappen do for my retail business?</p>
             <h1>We drive more <span class="text-blue">foot traffic</span> and turn your customers to be <span class="text-blue">influencers</span></h1>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span4"><img src="assets/images/howto/sh-slide-06.png" alt=""><br /><h4>Reach out and motivate</h4><p>Reach out your marketing campaign. We tell our users what’s more to walk in when they go shopping near your store. Motivate them to visit your store through challenges and rewards system.</p></div>
-                    <div class="span4"><img src="assets/images/howto/sh-slide-07.png" alt=""><br /><h4>Connect and engage</h4><p>The purpose of a business is to create a customer who creates customers. We allow you to engage your digital presence and marketing activities with your customers easily.</p></div>
-                    <div class="span4"><img src="assets/images/howto/sh-slide-08.png" alt=""><br /><h4>Customer insights</h4><p>Good businesses learn from customers. Great ones act on it. Know who is your customers and engage them with Socialhappen now.</p></div>
+                    <div class="span4"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-06.png" alt=""><br /><h4>Reach out and motivate</h4><p>Reach out your marketing campaign. We tell our users what’s more to walk in when they go shopping near your store. Motivate them to visit your store through challenges and rewards system.</p></div>
+                    <div class="span4"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-07.png" alt=""><br /><h4>Connect and engage</h4><p>The purpose of a business is to create a customer who creates customers. We allow you to engage your digital presence and marketing activities with your customers easily.</p></div>
+                    <div class="span4"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-08.png" alt=""><br /><h4>Customer insights</h4><p>Good businesses learn from customers. Great ones act on it. Know who is your customers and engage them with Socialhappen now.</p></div>
                 </div>
             </div>
         </div>
 
         <!-- Slide 05 -->
-        <div class="step slide" data-x="-1000" data-y="2500" data-z="-400" data-rotate="225">
+        <div class="step slide" data-x="-2000" data-y="2500" data-rotate="180">
             <p class="subheading">1. We motivate customers to visit more through types of rewards system</p>
             <h1>People just love rewards!</h1>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span3"><img src="assets/images/howto/sh-slide-01.png" alt=""></div>
-                    <div class="span3"><img src="assets/images/howto/sh-slide-03.png" alt=""></div>
-                    <div class="span3"><img src="assets/images/howto/sh-slide-04.png" alt=""></div>
-                    <div class="span3"><img src="assets/images/howto/sh-slide-05.png" alt=""></div>
+                    <div class="span3"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-01.png" alt=""></div>
+                    <div class="span3"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-03.png" alt=""></div>
+                    <div class="span3"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-04.png" alt=""></div>
+                    <div class="span3"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-05.png" alt=""></div>
                 </div>
                 <div class="row-fluid">
                     <div class="span3"><h4>Real rewarding machine</h4></div>
@@ -115,17 +115,17 @@
         </div>
 
         <!-- Slide 06 -->
-        <div class="step slide" data-x="-2000" data-y="1500" data-z="-500" data-rotate="270">
+        <div class="step slide" data-x="-2000" data-y="1500" data-rotate="180">
             <p class="subheading">2. And engage your customers to connect to your store through our..</p>
             <h1>Retail Engagement Platform</h1>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span5"><img src="assets/images/howto/sh-slide-09.png" alt=""></div>
+                    <div class="span5"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-09.png" alt=""></div>
                     <div class="span7">
                         <p class="subheading">which you can mix & match your own.</p>
                         <h1 class="text-blue">Marketing Challenges</h1>
                         <p class="subheading">now you can let your customers connect to your social media, or marketing activities easily.</p>
-                        <ul class="subheading unstyled">
+                        <ul class="subheading unstyled text-light">
                             <li>1. Like company Fan Page</li>
                             <li>2. Motivate Check-in and Sharing</li>
                             <li>3. Give Feedback</li>
@@ -138,13 +138,13 @@
         </div>
 
         <!-- Slide 07 -->
-        <div class="step slide" data-x="-3000" data-y="500" data-z="-400" data-rotate="315">
+        <div class="step slide" data-x="-2000" data-y="500" data-rotate="180">
             <p class="subheading">2.1 Type of marketing actions</p>
             <h1>Create <spam class="text-blue">marketing actions</spam> that let your customers <span class="text-blue">engage</span> with your brand</h1>
             <h2>Type of Challenge Actions</h2>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span12"><img src="assets/images/howto/sh-slide-10.png" alt=""></div>
+                    <div class="span12"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-10.png" alt=""></div>
                 </div>
                 <div class="row-fluid">
                     <div class="span3 align-center"><h4>Like your page</h4></div>
@@ -156,28 +156,28 @@
         </div>
 
         <!-- Slide 08 -->
-        <div class="step slide" data-x="-4000" data-y="-1000" data-z="-300" data-rotate="360">
+        <div class="step slide" data-x="-2000" data-y="-1000" data-rotate="180">
             <p class="subheading">3. Which will instantly deliver to your customer’s hand by just scanning the Socialhappen app at your store. This magic can happen thanks to..</p>
             <h1>OUR TRANSMISSION TECHNOLOGY</h1>
             <p class="subheading">that helps deliver marketing challenge to your real world customers easily.</p>
             <div class="container">
                 <div class="row-fluid align-center">
                     <div class="span1"></div>
-                    <img class="span10" src="assets/images/howto/sh-slide-11.png" alt="">
+                    <img class="span10" src="<?php echo base_url(); ?>assets/images/howto/sh-slide-11.png" alt="">
                 </div>
             </div>
         </div>
 
         <!-- Slide 09 -->
-        <div class="step slide" data-x="-5000" data-y="-2000" data-z="-200" data-rotate="405">
+        <div class="step slide" data-x="-2000" data-y="-2500" data-rotate="180">
             <p class="subheading">4. As a results, you can turn customers to be influencers who will bring you more happy customers, and help build your brand.</p>
             <h1>The purpose of a business is to create <span class="text-blue">a customer who creates customers</span></h1>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span6"><img src="assets/images/howto/sh-slide-12.png" alt=""></div>
+                    <div class="span6"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-12.png" alt=""></div>
                     <div class="span6">
                         <h4>With Socialhappen, you can</h4>
-                        <ol class="subheading">
+                        <ol class="subheading text-light">
                             <li>Motivate real walk-ins</li>
                             <li>Bring every happy moment to your company social media</li>
                             <li>Connect to your customers and learn from them</li>
@@ -188,15 +188,15 @@
         </div>
 
         <!-- Slide 10 -->
-        <div class="step slide" data-x="-6000" data-y="-3000" data-z="-100" data-rotate="450">
+        <div class="step slide" data-x="-2500" data-y="-3500" data-rotate="90">
             <h1>If you think SocialHappen<br> can help your business.</h1>
             <div class="container">
                 <div class="row-fluid">
-                    <div class="span6"><img src="assets/images/howto/sh-slide-13.png" alt=""></div>
+                    <div class="span6"><img src="<?php echo base_url(); ?>assets/images/howto/sh-slide-13.png" alt=""></div>
                     <div class="span6">
                         <h4>Contact our</h4>
                         <p class="subheading">“Retail Engagement Specialist” <br>to start something. It’s easy.</p>
-                        <a href="#" class="btn btn-primary btn-large">Contact us</a>
+                        <a href="mailto:support@socialhappen.com?Subject=Hello%20SocialHappen" class="btn btn-primary btn-large">Contact us</a>
                         <br>
                         <br>
                         <p>
@@ -208,12 +208,11 @@
         </div>
 
         <!-- Slide 11 -->
-        <div class="step slide" data-x="-7000" data-y="-4000" data-z="0" data-rotate="495">
+        <div class="step slide" data-x="-1000" data-y="-3500" data-rotate="0">
             <h1>See it in action. How Socialhappen can help make engaging retail experience?</h1>
             <div class="container">
                 <div class="row-fluid play-vdo">
-                    <img src="assets/images/howto/sh-slide-14.png" alt="">
-                    <div class="btn-play" href="http://www.youtube.com/embed/r1lnJvxahoU?rel=0&autoplay=1&hd=1&vq=hd720"></div>
+                    <iframe width="853" height="480" src="http://www.youtube.com/embed/r1lnJvxahoU" frameborder="0" allowfullscreen></iframe>
                 </div>
             </div>
         </div>
@@ -227,26 +226,39 @@
         <div class="right slide-control" id="next">›</div>
     </div>
 
-    <script src="assets/js/common/jquery-1.9.1.min.js"></script>
-    <script src="assets/js/common/impress.js"></script>
-    <script src="assets/js/common/fancybox/jquery.fancybox-2.1.4.pack.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/common/jquery-1.9.1.min.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/common/impress.js"></script>
+    <script src="<?php echo base_url(); ?>assets/js/common/fancybox/jquery.fancybox-2.1.4.pack.js"></script>
     <script>
     $(function() {
         // Slide control
         impress().init();
-        $('#prev').click(impress().prev);
-        $('#next').click(impress().next);
+        $('#prev').click(function () {
+            var slide_index = parseInt(location.hash.replace('#/step-','')) - 1;
+            if($('.slide').eq(slide_index).length) {
+                impress().goto(slide_index-1);
+            }
+        });
+        $('#next').click(function () {
+            var slide_index = parseInt(location.hash.replace('#/step-',''));
+            if($('.slide').eq(slide_index).length) {
+                impress().goto(slide_index);
+            } else {
+                impress().goto(0);
+            }
+        });
 
         // Menu bar
         $('div.bot-nav a').click(function () {
+            console.log('set active');
             $(this).addClass('active').parent().siblings().find('a').removeClass('active');
         });
 
         // On slide change, active menu bar
         window.onhashchange = function () {
-            var slide_id = location.hash.replace('/','');
-            if($(slide_id).length) {
-                $(slide_id).addClass('active').parent().siblings().find('a').removeClass('active');
+            var slide_hash = location.hash.replace('/','');
+            if($(slide_hash).length) {
+                $(slide_hash).addClass('active').parent().siblings().find('a').removeClass('active');
             }
         };
 
